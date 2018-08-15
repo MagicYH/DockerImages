6770e4e15eff5335e32cecc36632b0bce7b17b07
MagicYH <magic8806@gmail.com>
2018-08-15 12:03:31 +0800

    Update cmd.sh

--- nginx-php/cmd.sh
+++ nginx-php/cmd.sh
@@ -7,12 +7,23 @@ mkdir -p /data/php/log
 cat <<EOF > nginx.repo
 [nginx]
 name=nginx repo
-baseurl=http://nginx.org/packages/centos/7/$basearch/
+baseurl=http://nginx.org/packages/centos/7/\$basearch/
 gpgcheck=0
 enabled=1
 EOF
 
-yum install -y make gcc g++ autoconf wget nginx epel-release supervisor libxml2-devel openssl-devel libcurl-devel gd-devel
+yum install -y epel-release
+yum install -y make
+yum install -y gcc
+yum install -y g++
+yum install -y autoconf
+yum install -y wget
+yum install -y nginx
+yum install -y supervisor
+yum install -y libxml2-devel
+yum install -y openssl-devel
+yum install -y libcurl-devel
+yum install -y gd-devel
 
 cd /tmp
 wget http://cn2.php.net/distributions/php-7.2.8.tar.gz -O php-7.2.8.tar.gz
@@ -67,6 +78,7 @@ sed -i 's/pm.start_servers = .*/pm.start_servers = 2/' /usr/local/php/etc/php-fp
 sed -i 's/pm.min_spare_servers = .*/pm.min_spare_servers = 2/' /usr/local/php/etc/php-fpm.d/www.conf.default
 sed -i 's/pm.max_spare_servers = .*/pm.max_spare_servers = 4/' /usr/local/php/etc/php-fpm.d/www.conf.default
 sed -i 's/catch_workers_output = .*/catch_workers_output = yes/' /usr/local/php/etc/php-fpm.d/www.conf.default
+cp /usr/local/php/etc/php-fpm.d/www.conf.default /usr/local/php/etc/php-fpm.d/www.conf
 sed -i 's/;error_log = .*/error_log = \/tmp\/php_error.log/' /usr/local/php/etc/php.ini
 echo 'extension=igbinary.so' >> /usr/local/php/etc/php.ini
 echo 'extension=redis.so' >> /usr/local/php/etc/php.ini
@@ -75,3 +87,21 @@ sed -i '1,3d' /etc/nginx/nginx.conf
 sed -i '1i\daemon off;' /etc/nginx/nginx.conf
 sed -i '1i\worker_processes 2;' /etc/nginx/nginx.conf
 sed -i '1i\user linux;' /etc/nginx/nginx.conf
+
+cat <<EOF > /etc/supervisord.d/default.ini
+[php-fpm]
+command=/usr/local/php/sbin/php-fpm -F
+user=linux
+autorestart=true
+redirect_stderr=true
+stdout_logfile=/data/php/log/php-fpm.log
+
+[nginx]
+command=nginx
+user=linux
+autorestart=true
+redirect_stderr=true
+stdout_logfile=/data/nginx/log/nginx.log
+EOF
+
+yum clean all
